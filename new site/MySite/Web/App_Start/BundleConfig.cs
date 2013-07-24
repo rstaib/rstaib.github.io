1f78e57f409ec5117762474a642a451a9554f1b6
Rafael J. Staib <rstaib@derklang.com>
2013-07-24 14:28:31 +0200

    Add first version of the new site

---
+++ new site/MySite/Web/App_Start/BundleConfig.cs
@@ -0,0 +1,49 @@
+﻿using dotless.Core;
+using dotless.Core.Input;
+using dotless.Core.configuration;
+using System.Web.Optimization;
+
+namespace MySite.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/cdn/scripts/base")
+                .Include("~/cdn/scripts/jquery-{version}.js")
+                .Include("~/cdn/scripts/bootstrap.js")
+                .Include("~/cdn/scripts/bootstrap-*"));
+
+            bundles.Add(new ScriptBundle("~/cdn/Scripts/validation")
+                .Include("~/cdn/scripts/jquery.validate*"));
+
+            var less = new Bundle("~/cdn/styles/base")
+                .Include("~/cdn/content/bootstrap*")
+                .Include("~/cdn/content/site.less");
+            less.Transforms.Add(new LessTransform());
+            less.Transforms.Add(new CssMinify());
+            bundles.Add(less);
+        }
+
+        public class LessTransform
+            : IBundleTransform
+        {
+            public void Process(BundleContext context, BundleResponse response)
+            {
+                var config = new DotlessConfiguration
+                {
+                    MinifyOutput = false,
+                    ImportAllFilesAsLess = true,
+                    CacheEnabled = false,
+                    //#if DEBUG
+                    //Logger = typeof (DiagnosticsLogger),
+                    //#endif
+                    LessSource = typeof(VirtualFileReader)
+                };
+
+                response.Content = Less.Parse(response.Content, config);
+                response.ContentType = "text/css";
+            }
+        }
+    }
+}
